29ef0119839ada887b3d119fae6668bee3e94009
Al Dynak <adynak@gmail.com>
2020-05-13 14:56:53 -0700

    adding search and helper functions

---
+++ wineApp/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  wineApp
+//
+//  Created by adynak on 5/11/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
---
+++ wineApp/temp.swift
@@ -0,0 +1,9 @@
+//
+//  temp.swift
+//  wineApp
+//
+//  Created by adynak on 5/12/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
