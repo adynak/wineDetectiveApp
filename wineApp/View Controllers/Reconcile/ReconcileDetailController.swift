c75db7b69f3cd0af498b6b404816bd3a43e81656
Al Dynak <adynak@gmail.com>
2020-07-01 11:49:17 -0700

    clone reconcile to producer

--- wineApp/View Controllers/Reconcile/ReconcileDetailController.swift
+++ wineApp/View Controllers/Reconcile/ReconcileDetailController.swift
@@ -1,9 +1,284 @@
 //
-//  ReconcileDetailController.swift
+//  ReconcileViewDetailController.swift
 //  wineApp
 //
-//  Created by adynak on 7/1/20.
-//  Copyright © 2020 Al Dynak. All rights reserved.
+//  Created by adynak on 12/6/18.
+//  Copyright © 2018 Al Dynak. All rights reserved.//
 //
 
-import Foundation
+import UIKit
+
+class ReconcileViewDetailController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+        
+    var tableContainerTopAnchor:CGFloat = 200.0
+    var tableContainerHeightAnchor:CGFloat = UIScreen.main.bounds.height - 327
+//    let tableRowHeight:CGFloat = 60
+    let sortBins:Bool = true
+    
+    let cellID = "cellId"
+    var wineBins = [DrillLevel2]()
+    
+    var cells = [ReconcileTableViewCell]() //initialize array at class level
+
+    var passedValue = wineDetail()
+    
+    let storageLabel: UITextView = {
+        let tv = UITextView()
+        tv.text = NSLocalizedString("locationAndBin", comment: "")
+        tv.font = UIFont.boldSystemFont(ofSize: 14)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor(r:202, g:227, b:255)
+        tv.textAlignment = .left
+        tv.isEditable = false
+        tv.isScrollEnabled = true
+        return tv
+    }()
+
+    let inventoryFooter: UITextView = {
+        let tv = UITextView()
+        tv.font = UIFont.boldSystemFont(ofSize: 18)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.textAlignment = .left
+        tv.isEditable = false
+        tv.isScrollEnabled = false
+        tv.layer.cornerRadius = 5
+        tv.layer.masksToBounds = true
+        tv.text = NSLocalizedString("totalBottles", comment: "")
+        tv.contentInset=UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0);
+        return tv
+    }()
+    
+    let tableContainer: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.layer.cornerRadius = 5
+        return v
+    }()
+    
+    let wineBinsTableView : UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.layer.cornerRadius = 5
+        return tv
+    }()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        wineBins = passedValue.bottles!
+
+        view.backgroundColor = UIColor(r:202, g:227, b:255)
+        view.addSubview(storageLabel)
+        view.addSubview(tableContainer)
+        tableContainer.addSubview(wineBinsTableView)
+        view.addSubview(inventoryFooter)
+
+        setupNavigationBar()
+        setupWineLabelLayout()
+        setupInventoryFooterLayout()
+        setupWineBinsTableViewLayout()
+        
+    }
+    
+    func setupInventoryFooterLayout(){
+        inventoryFooter.text = getTotalBottles()
+        NSLayoutConstraint.activate([
+            inventoryFooter.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            inventoryFooter.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            inventoryFooter.heightAnchor.constraint(equalToConstant: 40),
+            inventoryFooter.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant : 6),
+            inventoryFooter.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant : -6)
+        ])
+    }
+    
+    func setupWineLabelLayout(){
+        let locationAndBin = ""
+        let bottles = NSLocalizedString("bottles", comment: "")
+        storageLabel.text = "\(locationAndBin)\(passedValue.location!) \(passedValue.bin!)\n\(bottles): \(passedValue.bottleCount)"
+        
+        NSLayoutConstraint.activate([
+            storageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            storageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant : 5.0),
+            storageLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24),
+            storageLabel.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    func setupWineBinsTableViewLayout(){
+        tableContainer.backgroundColor = .white
+        NSLayoutConstraint.activate([
+            tableContainer.topAnchor.constraint(equalTo:storageLabel.bottomAnchor, constant:10),
+            tableContainer.leadingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leadingAnchor, constant : 6),
+            tableContainer.trailingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.trailingAnchor, constant : -6),
+            tableContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -110)
+        ])
+        
+        NSLayoutConstraint.activate([
+            wineBinsTableView.topAnchor.constraint(equalTo:tableContainer.topAnchor, constant: 5),
+            wineBinsTableView.leadingAnchor.constraint(equalTo:tableContainer.leadingAnchor, constant: 5),
+            wineBinsTableView.trailingAnchor.constraint(equalTo:tableContainer.trailingAnchor, constant: -5),
+            wineBinsTableView.bottomAnchor.constraint(equalTo:tableContainer.bottomAnchor, constant: -5)
+        ])
+        
+        wineBinsTableView.dataSource = self
+        wineBinsTableView.register(ReconcileTableViewCell.self, forCellReuseIdentifier: cellID)
+        if showBarcode {
+            wineBinsTableView.rowHeight = CGFloat(80)
+        } else {
+            wineBinsTableView.rowHeight = CGFloat(80)
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wineBins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // want to stripe the table's rows?
+        let colorOdd = UIColor(r:255, g:255, b:255) //white
+        let colorEven = UIColor(r:240, g:240, b:240)
+        
+        let vintage = wineBins[indexPath.row].vintage
+        let producer = wineBins[indexPath.row].producer
+        let varietal = wineBins[indexPath.row].varietal
+        let iWine = wineBins[indexPath.row].iWine
+        let barcode = wineBins[indexPath.row].barcode
+        
+        let location = wineBins[indexPath.row].location
+        let bin = wineBins[indexPath.row].bin
+        
+        let designation = wineBins[indexPath.row].designation
+        let ava = wineBins[indexPath.row].ava
+        let beginConsume = wineBins[indexPath.row].beginConsume
+        let endConsume = wineBins[indexPath.row].endConsume
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! ReconcileTableViewCell
+        
+        cell.bin = DrillLevel2(
+                    producer: producer,
+                    varietal: varietal,
+                    vintage: vintage,
+                    iWine: iWine,
+                    location: location,
+                    bin: bin,
+                    barcode: barcode,
+                    designation: designation,
+                    ava: ava,
+                    beginConsume: beginConsume,
+                    endConsume: endConsume)
+        cell.backgroundColor = indexPath.row % 2 == 0 ? colorOdd : colorEven
+        cell.delegate = self
+        
+        if(!cells.contains(cell)){
+            self.cells.append(cell)
+        }
+
+        return cell
+    }
+    
+    private func setupNavigationBar(){
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.title = title
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title:   NSLocalizedString("cancel", comment: ""),
+            style: .plain,
+            target: self,
+            action: #selector(addTapped))
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title:   NSLocalizedString("save", comment: ""),
+            style: .plain,
+            target: self,
+            action: #selector(addTapped))
+    }
+    
+    @objc func addTapped(sender: UIBarButtonItem){
+        var markAsDrank = [Level2]()
+
+        if (sender.title == NSLocalizedString("save", comment: "")){
+            for cell in cells {
+                if let bottles = Int.parse(from: cell.bottleCountLabel.text!) {
+                    if bottles == 0{
+                        markAsDrank.append(Level2(
+                            producer: cell.producerLabel.text!,
+                            varietal: cell.locationAndBinLabel.text!,
+                            vintage: cell.vintageLabel.text!,
+                            iWine: cell.iWineLabel.text!,
+                            barcode: cell.barcodeLabel.text!))
+                    }
+                }
+            }
+            if markAsDrank.count > 0{
+                let alertTitle = NSLocalizedString("markAsDrankTitle", comment: "")
+                let okBtn = NSLocalizedString("okBtn", comment: "")
+                let cancelBtn = NSLocalizedString("cancelBtn", comment: "")
+//                let message = buildRemoveMessage(bottles: markAsDrank)
+                let message = ""
+
+                let markAsDrankAlert = UIAlertController.init(title: alertTitle, message: message, preferredStyle:.alert)
+
+                markAsDrankAlert.addAction(UIAlertAction.init(title: okBtn, style: .default) { (UIAlertAction) -> Void in
+                    self.dismiss(animated: true, completion:{
+                        DataServices.removeBottles(bottles: markAsDrank)
+                    })
+                })
+
+                markAsDrankAlert.addAction(UIAlertAction.init(title: cancelBtn, style: .cancel, handler: nil))
+
+                present(markAsDrankAlert, animated: true, completion: nil)
+
+            } else {
+                dismiss(animated: true, completion: nil)
+            }
+
+        } else {
+            dismiss(animated: true, completion: nil)
+        }
+    }
+        
+    private func getTotalBottles()  -> String {
+        let singularBottle = NSLocalizedString("singularBottle", comment: "")
+        let pluralBottle = NSLocalizedString("pluralBottle", comment: "")
+        let totalBottles = wineBins.count
+        let bottleString = (totalBottles > 1) ? pluralBottle : singularBottle
+        
+        return String(totalBottles) + bottleString
+    }
+    
+    func buildRemoveMessage(bottles: [DrillLevel2])->String{
+        let confirmBtn = NSLocalizedString("confirmBtn", comment: "")
+
+        var message: String = ""
+        for bottle in bottles{
+            message += "\(bottle.vintage!) \(bottle.producer!)\n \(bottle.varietal!)\n\n"
+        }
+        message += confirmBtn
+        return message
+    }
+
+}
+
+extension ReconcileViewDetailController : ReconcileBinCellDelegate {
+    func didTapStepper(direction: String){
+        let singularBottle = NSLocalizedString("singularBottle", comment: "")
+        let pluralBottle = NSLocalizedString("pluralBottle", comment: "")
+        let minus = "minus" as String
+        let plus = "plus" as String
+        
+        var count = 0
+        
+        let number = Int.parse(from: inventoryFooter.text!)
+        if (direction == minus){
+            count = number! - 1
+        }
+        if (direction == plus){
+            count = number! + 1
+        }
+        let bottleString = (count == 1) ? singularBottle : pluralBottle
+
+        inventoryFooter.text = String(count) + bottleString
+    }
+}
--- wineApp/View Controllers/Reconcile/ReconcileTableViewCell.swift
+++ wineApp/View Controllers/Reconcile/ReconcileTableViewCell.swift
@@ -1,9 +1,222 @@
 //
-//  ReconcileTableViewCell.swift
-//  wineApp
+//  BinTableViewCell.swift
+//  wineBinsTableView
 //
-//  Created by adynak on 7/1/20.
-//  Copyright © 2020 Al Dynak. All rights reserved.
+//  Created by adynak on 12/20/18.
+//  Copyright © 2018 adynak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ReconcileBinCellDelegate{
+    func didTapStepper(direction: String)
+}
+
+class ReconcileTableViewCell : UITableViewCell {
+    
+    let singularText = NSLocalizedString("singularBottle", comment: "")
+    let pluralText = NSLocalizedString("pluralBottle", comment: "plural")
+    
+    var delegate: ReconcileBinCellDelegate?
+
+    var bin:DrillLevel2! {
+        didSet {
+            guard let binItem = bin else {return}
+
+            if let producer = binItem.producer {
+                let designation = binItem.designation!
+                let ava = binItem.ava!
+                let description = designation == "" ? ava : "\(designation) \(ava)"
+                vintageAndDescriptionLabel.text =  "\(binItem.vintage!) \(description) "
+                producerLabel.text = producer
+            }
+            
+            locationAndBinLabel.text = "Location: \(binItem.location!) \(binItem.bin!)"
+            barcodeLabel.text = "Barcode: \(binItem.barcode!)"
+            let drinkBy = buildDrinkBy(beginConsume: binItem.beginConsume!, endConsume: binItem.endConsume!)
+            drinkByLabel.text = "Drinking Window: \(drinkBy)"
+
+            let bottleCount: Int = 1
+            bottleCountLabel.text = setLabelText(count:bottleCount)
+            
+            stepperView.value = Double(bottleCount)
+            stepperView.tag = bottleCount
+            iWineLabel.text = binItem.iWine
+            vintageLabel.text = binItem.vintage
+        }
+    }
+    
+    let vintageAndDescriptionLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let iWineLabel:UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        return label
+    }()
+    
+    let barcodeLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isHidden = !showBarcode
+        return label
+    }()
+
+    let drinkByLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let vintageLabel:UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        return label
+    }()
+    
+    let producerLabel:UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        return label
+    }()
+
+    let locationAndBinLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let bottleCountLabel:UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.italicSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let containerView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        return view
+    }()
+
+    let stepperView : UIStepper = {
+        let s = UIStepper()
+        s.translatesAutoresizingMaskIntoConstraints = false
+        s.minimumValue = 0
+        s.maximumValue = 1
+        s.value = 1
+        return s
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        containerView.addSubview(vintageAndDescriptionLabel)
+        containerView.addSubview(drinkByLabel)
+        containerView.addSubview(locationAndBinLabel)
+        
+        if showBarcode {
+            containerView.addSubview(barcodeLabel)
+        }
+        
+        containerView.addSubview(bottleCountLabel)
+        
+        contentView.addSubview(containerView)
+        contentView.addSubview(stepperView)
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo:self.centerYAnchor),
+            containerView.leadingAnchor.constraint(equalTo:self.leadingAnchor, constant:0),
+            containerView.trailingAnchor.constraint(equalTo:self.trailingAnchor, constant:-10),
+            containerView.heightAnchor.constraint(equalToConstant:80)
+        ])
+        
+        NSLayoutConstraint.activate([
+            vintageAndDescriptionLabel.topAnchor.constraint(equalTo:containerView.topAnchor),
+            vintageAndDescriptionLabel.leadingAnchor.constraint(equalTo:containerView.leadingAnchor, constant: 10),
+            vintageAndDescriptionLabel.heightAnchor.constraint(equalToConstant:12),
+            vintageAndDescriptionLabel.trailingAnchor.constraint(equalTo:containerView.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            drinkByLabel.topAnchor.constraint(equalTo:vintageAndDescriptionLabel.bottomAnchor),
+            drinkByLabel.leadingAnchor.constraint(equalTo:vintageAndDescriptionLabel.leadingAnchor, constant: 10),
+            drinkByLabel.heightAnchor.constraint(equalToConstant:14),
+            drinkByLabel.trailingAnchor.constraint(equalTo:containerView.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            locationAndBinLabel.topAnchor.constraint(equalTo:drinkByLabel.bottomAnchor),
+            locationAndBinLabel.leadingAnchor.constraint(equalTo:drinkByLabel.leadingAnchor, constant: 10),
+            locationAndBinLabel.heightAnchor.constraint(equalToConstant:14),
+            locationAndBinLabel.trailingAnchor.constraint(equalTo:containerView.trailingAnchor)
+        ])
+        
+        if showBarcode {
+            NSLayoutConstraint.activate([
+                barcodeLabel.topAnchor.constraint(equalTo:locationAndBinLabel.bottomAnchor),
+                barcodeLabel.leadingAnchor.constraint(equalTo:drinkByLabel.leadingAnchor, constant: 10),
+                barcodeLabel.heightAnchor.constraint(equalToConstant:14),
+                barcodeLabel.trailingAnchor.constraint(equalTo:containerView.trailingAnchor)
+            ])
+            
+            NSLayoutConstraint.activate([
+                bottleCountLabel.topAnchor.constraint(equalTo:barcodeLabel.bottomAnchor),
+                bottleCountLabel.leadingAnchor.constraint(equalTo:vintageAndDescriptionLabel.leadingAnchor, constant: 10),
+                barcodeLabel.heightAnchor.constraint(equalToConstant:14),
+                barcodeLabel.trailingAnchor.constraint(equalTo:containerView.trailingAnchor)
+            ])
+                        
+        } else {
+            NSLayoutConstraint.activate([
+                bottleCountLabel.topAnchor.constraint(equalTo:locationAndBinLabel.bottomAnchor),
+                bottleCountLabel.leadingAnchor.constraint(equalTo:vintageAndDescriptionLabel.leadingAnchor, constant: 10)
+            ])
+        }
+                
+        NSLayoutConstraint.activate([
+            stepperView.centerYAnchor.constraint(equalTo:self.centerYAnchor),
+            stepperView.leadingAnchor.constraint(equalTo:self.trailingAnchor, constant:-100),
+            stepperView.trailingAnchor.constraint(equalTo:self.trailingAnchor, constant:-10)
+        ])
+        
+        stepperView.addTarget(self, action: #selector(stepperAction), for: .valueChanged)
+
+    }
+    
+    private func setLabelText(count: Int) -> String {
+        let plural = count == 1 ? singularText : pluralText
+        return String(count) + plural
+    }
+
+    @objc func stepperAction(sender: UIStepper)  {
+        
+        var responseMessages = ["direction": "OK"]
+        
+        let stepDirection = sender.tag > Int(sender.value) ? "minus" : "plus"
+        sender.tag = Int(sender.value)
+        bottleCountLabel.text = setLabelText(count: Int(sender.value))
+                
+        responseMessages["direction"] = stepDirection
+        delegate?.didTapStepper(direction: stepDirection)
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
