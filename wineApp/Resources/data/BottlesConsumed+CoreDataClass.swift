1a9fa5f12ca1fb62dad3f90fd931d9b2353d42af
Al Dynak <adynak@gmail.com>
2020-08-17 09:55:33 -0700

    add coreData for consumedBottles

---
+++ wineApp/Resources/data/BottlesConsumed+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BottlesConsumed+CoreDataClass.swift
+//  wineApp
+//
+//  Created by adynak on 8/17/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class BottlesConsumed: NSManagedObject {
+
+}
