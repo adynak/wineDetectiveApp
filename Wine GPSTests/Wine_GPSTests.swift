c0fe293be193ceb1e33521c4aad0e3b433a8ade9
Al Dynak <adynak@gmail.com>
2020-08-22 08:25:17 -0700

    unit tests 101

---
+++ Wine GPSTests/Wine_GPSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wine_GPSTests.swift
+//  Wine GPSTests
+//
+//  Created by adynak on 8/22/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import XCTest
+
+class Wine_GPSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
