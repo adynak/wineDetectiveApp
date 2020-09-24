a1b8103d1edc36490f6419c9022cc5570b4bd12c
Al Dynak <adynak@gmail.com>
2020-09-24 14:52:06 -0700

    adding intent provider

---
+++ WineGPS/Models/VarietalDetail.swift
@@ -0,0 +1,9 @@
+//
+//  VarietalDetail.swift
+//  WineGPS
+//
+//  Created by adynak on 9/24/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
---
+++ WineGPSWidget/WineGPSBundle.swift
@@ -0,0 +1,9 @@
+//
+//  WineGPSBundle.swift
+//  WineGPS
+//
+//  Created by adynak on 9/24/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
