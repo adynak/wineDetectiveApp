078fb9e9a11fcf57928fe34f8729d79ae44469be
Al Dynak <adynak@gmail.com>
2020-10-08 08:48:50 -0700

    reposition icloud alert

--- WineGPS/AppDelegate.swift
+++ WineGPS/AppDelegate.swift
@@ -45,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
                 
-        
         let group = DispatchGroup()
         group.enter()
         DispatchQueue.global(qos: .default).async {
