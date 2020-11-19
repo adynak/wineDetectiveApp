46c4ff2aa85bba28350a0b25c6254f71a07f094f
Al Dynak <adynak@gmail.com>
2020-11-19 10:04:11 -0800

    moving MainTabBarController

---
+++ WineGPS/Models/CloudKit.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKit.swift
+//  WineGPS
+//
+//  Created by adynak on 11/19/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
---
+++ WineGPS/View Controllers/MainTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarController.swift
+//  WineGPS
+//
+//  Created by adynak on 11/19/20.
+//  Copyright © 2020 Al Dynak. All rights reserved.
+//
+
+import Foundation
