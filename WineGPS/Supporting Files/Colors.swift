99f1050edf2c839c4a58d22ff975d7f170a493e5
Al Dynak <adynak@gmail.com>
2020-11-18 17:33:34 -0800

    color extension issues on archive

--- WineGPS/Supporting Files/Colors.swift
+++ WineGPS/Supporting Files/Colors.swift
@@ -29,4 +29,38 @@ let drinkByMenuBackground =       UIColor(r: 209, g: 209, b: 209)
 let notNowButton =                UIColor(r:  94, g: 215, b: 206)
 let unDrinkButton =               UIColor(r: 167, g: 148, b: 240)
 
+//extension UIColor {
+//    public convenience init?(hex: String) {
+//        let r, g, b, a: CGFloat
+//
+//        if hex.hasPrefix("#") {
+//            let start = hex.index(hex.startIndex, offsetBy: 1)
+//            let hexColor = String(hex[start...])
+//
+//            if hexColor.count == 8 {
+//                let scanner = Scanner(string: hexColor)
+//                var hexNumber: UInt64 = 0
+//
+//                if scanner.scanHexInt64(&hexNumber) {
+//                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+//                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+//                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+//                    a = CGFloat(hexNumber & 0x000000ff) / 255
+//
+//                    self.init(red: r, green: g, blue: b, alpha: a)
+//                    return
+//                }
+//            }
+//        }
+//
+//        return nil
+//    }
+//}
 
+extension UIColor {
+    
+    public convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+}
